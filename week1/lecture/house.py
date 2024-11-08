540cd51e2f0313c10c8014788c4188f09b7dfeaa
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-11-08 19:49:13 -0300

    Create house.py

---
+++ week1/lecture/house.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slyntherin")
+    case _:
+        print("Who?")
