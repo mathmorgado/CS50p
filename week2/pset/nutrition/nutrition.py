f7bf83daea2f84ad1c2a7c6dafba5821e643751b
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-11-09 15:04:40 -0300

    Create nutrition.py

---
+++ week2/pset/nutrition/nutrition.py
@@ -0,0 +1,35 @@
+def main():
+    fruit = input("Fruit: ")
+    print(get_calories(fruit))
+
+
+def get_calories(fruit):
+    fruit_calories = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+    }
+
+    calories = fruit_calories.get(fruit.lower(), "")
+
+    return f"Calories: {calories}" if calories != "" else ""
+
+
+main()
